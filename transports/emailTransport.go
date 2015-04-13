62f3f75ebe5ee906c9f0dff3a223d0fd1537fbe3
Jason Michels <jmichels@fisdap.net>
2015-04-13 15:45:06 -0500

    Figured out issues with docker development environment

--- transports/emailTransport.go
+++ transports/emailTransport.go
@@ -12,7 +12,6 @@ type EmailTransport struct {
 // Send email using sendgrid, but this could be swapped out for any email implementation
 func (transport *EmailTransport) Send() {
     fmt.Println("Send email from email transport")
-    fmt.Println("Sending email to jmichels@fisdap.net")
 
     username := os.Getenv("SENDGRID_USERNAME")
     password := os.Getenv("SENDGRID_PASSWORD")
@@ -21,11 +20,11 @@ func (transport *EmailTransport) Send() {
     sg := sendgrid.NewSendGridClient(username, password)
     
     message := sendgrid.NewMail()
-    message.AddTo("jmichels@fisdap.net")
-    message.AddToName("Jason Michels")
+    message.AddTo("test@jasonmichels.com")
+    message.AddToName("Jason")
     message.SetSubject("SendGrid Testing")
     message.SetText("WIN")
-    message.SetFrom("jmichels@fisdap.net")
+    message.SetFrom("test@jasonmichels.com")
 
     if r := sg.Send(message); r == nil {
         fmt.Println("Email sent!")
